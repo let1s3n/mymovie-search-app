5c68f794350df35153f3ad3a6a4f22e38d7a7910
let1s3n <letisendev@gmail.com>
2021-04-04 01:10:33 -0500

    asaaaaasadsada

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
+console.log(process.env.API_KEY);
 if (process.env.NODE_ENV == 'development')
 require('dotenv').config({ silent: true });
 app.use(express.static(__dirname + '/dist'));
