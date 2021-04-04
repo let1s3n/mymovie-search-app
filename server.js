85708b19560375c614efd102849650d772b7779b
let1s3n <letisendev@gmail.com>
2021-04-04 01:00:05 -0500

    asdasdasd

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var path = require('path');
-var express = require('express');
-
-var app = express();
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
 if (process.env.NODE_ENV == 'development')
 require('dotenv').config({ silent: true });
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist'));
 
-
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+app.listen(port);
